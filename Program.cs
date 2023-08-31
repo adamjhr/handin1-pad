32844f13b063e39e4a0f95347729d9c818a2afde
adamjhr <adam.jh.rose6@gmail.com>
2023-08-31 14:33:00 +0200

    Almost done with 1.4

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using handin1_pad;
+
+Expr e1 = new Add(new CstI(17), new Var("z"));
+Expr e2 = new Mul(new Sub(new Var("a"), new Var("b")), new CstI(3));
+Expr e3 = new Add(new Var("x"), new Mul(new CstI(2), new Mul(new Var("x"), new Var("x"))));
+Expr e4 = new Sub(new Sub(new Sub(new Sub(new Var("x"), new Var("x")), new Var("x")), new Var("x")), new CstI(20));
+
+Console.WriteLine(e1.toString());
+Console.WriteLine(e2.toString());
+Console.WriteLine(e3.toString());
+Console.WriteLine(e4.toString());
+
---
+++ exercise_1_4.cs
@@ -0,0 +1,116 @@
+namespace handin1_pad;
+
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+abstract class Expr {
+
+    public abstract int eval(ImmutableDictionary<string, int> env);
+    public abstract Expr simplify();
+    public abstract string toString();
+
+}
+
+class CstI : Expr {
+    private int value;
+
+    public CstI(int value) {
+        this.value = value;
+    }
+
+    public override int eval(ImmutableDictionary<string, int> env) {
+        return value;
+    }
+
+    public override Expr simplify() {
+        return this;
+    }
+
+    public override string toString() {
+        return value.ToString();
+    }
+}
+
+class Var : Expr {
+    private string name;
+
+    public Var(string name) {
+        this.name = name;
+    }
+
+    public override int eval(ImmutableDictionary<string, int> env) {
+        return env[name];
+    }
+
+    public override Expr simplify() {
+        return this;
+    }
+
+    public override string toString() {
+        return name;
+    }
+}
+
+abstract class Binop : Expr {
+    protected Expr exp1;
+    protected Expr exp2;
+
+    protected Binop(Expr exp1, Expr exp2) {
+        this.exp1 = exp1;
+        this.exp2 = exp2;
+    }
+}
+class Add : Binop {
+
+    public Add(Expr exp1, Expr exp2) : base(exp1, exp2) {}
+
+    public override int eval(ImmutableDictionary<string, int> env) {
+        return exp1.eval(env) + exp2.eval(env);
+    }
+
+    // INCOMPLETE
+    public override Expr simplify() {
+        return new Add(exp1.simplify(), exp2.simplify());
+    }
+
+    public override string toString() {
+        return "(" + exp1.toString() + " + " + exp2.toString() + ")";
+    }
+} 
+
+class Mul : Binop {
+
+    public Mul(Expr exp1, Expr exp2) : base(exp1, exp2) {}
+
+    public override int eval(ImmutableDictionary<string, int> env) {
+        return exp1.eval(env) * exp2.eval(env);
+    }
+
+    // INCOMPLETE
+    public override Expr simplify() {
+        return new Mul(exp1.simplify(), exp2.simplify());
+    }
+
+    public override string toString() {
+        return "(" + exp1.toString() + " * " + exp2.toString() + ")";
+    }
+} 
+
+class Sub : Binop {
+
+    public Sub(Expr exp1, Expr exp2) : base(exp1, exp2) {}
+
+    public override int eval(ImmutableDictionary<string, int> env) {
+        return exp1.eval(env) - exp2.eval(env);
+    }
+
+    // INCOMPLETE
+    public override Expr simplify() { 
+        return new Sub(exp1.simplify(), exp2.simplify());
+    }
+
+    public override string toString() {
+        return "(" + exp1.toString() + " - " + exp2.toString() + ")";
+    }
+}
+
