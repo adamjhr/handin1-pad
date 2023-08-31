ff435f55b6fbbb8984080c75ced12c8b07b0aae2
adamjhr <adam.jh.rose6@gmail.com>
2023-08-31 11:48:03 +0200

    More exercises...

--- Intro2.fs
+++ Intro2.fs
@@ -62,10 +62,61 @@ let e2v2 = eval e2 [("a", 314)];;
 let e3v  = eval e3 env
 
 
-//Exercise 1.1 b)
+// Exercise 1.1 b)
 (*
 eval (Prim("==", CstI 7, CstI 12)) [] -> 0
 eval (Prim("==", CstI 7, CstI 7)) [] -> 1
 eval (Prim("min", CstI 7, CstI 12)) [] -> 7
 eval (Prim("max", CstI 7, CstI 12)) [] -> 12
 *)
+
+// Exercise 1.2 i
+
+type aexp = 
+  | CstI of int
+  | Var of string
+  | Add of aexp * aexp
+  | Mul of aexp * aexp
+  | Sub of aexp * aexp
+
+// Exercise 1.2 ii
+
+let exp1 = Sub (Var "v", Add (Var "w", Var "z"))
+let exp2 = Mul (CstI 2, Sub (Var "v", Add (Var "w", Var "z")))
+let exp3 = Add (Var "x", Add (Var "y", Add (Var "z", Var "v")))
+
+// Exercise 1.2 iii
+
+let rec fmt (a : aexp) = 
+  match a with 
+  | CstI x -> string x
+  | Var x -> x
+  | Add (x, y) -> sprintf "(%s + %s)" (fmt x) (fmt y)
+  | Mul (x, y) -> sprintf "(%s * %s)" (fmt x) (fmt y)
+  | Sub (x, y) -> sprintf "(%s - %s)" (fmt x) (fmt y)
+
+// Exercise 1.2 iv
+
+let rec simplify (a : aexp) = 
+  match a with
+  | Add (CstI x, CstI y) -> CstI (x+y)
+  | Mul (CstI x, CstI y) -> CstI (x*y)
+  | Sub (CstI x, CstI y) -> CstI (x-y)
+  | Add (x, y) -> 
+    match (x, y) with  
+    | (CstI 0, y) -> y
+    | (x, CstI 0) -> x
+    | _ -> Add (x, y)
+  | Mul (x, y) -> 
+    match (x, y) with  
+    | (CstI 1, y) -> y
+    | (x, CstI 1) -> x
+    | (x, y) when x = CstI 0 || y = CstI 0 -> CstI 0
+    | _ -> Mul (x, y)
+  | Sub (x, y) -> 
+    match (x, y) with  
+    | (x, CstI 0) -> x
+    | (CstI x, CstI y) when x = y -> CstI 0
+    | (Var x, Var y) when x = y -> CstI 0 
+    | _ -> Sub (x, y)
+  | _ -> a
